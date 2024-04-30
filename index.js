60b1a8f55f76d8775443465146968b4b4b9d85af
Sumi <163683886+SumiFX@users.noreply.github.com>
2024-04-30 13:24:37 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var { aptoide, fbdl, GDriveDl, GoogleImage, igdl, igstalk, mediafire, openAi, rule34, tiktokdl, pinterest, ppcouple, spotifySearch, spotify, soundSearch, xnxxdl, ytsearch, ytmp3, ytmp4 } = require("./SumiFX")
+var { aptoide, fbdl, GDriveDl, GoogleImage, igdl, igstalk, mediafire, openAi, rule34, tiktokdl, pinterest, ppcouple, spotifySearch, spotify, soundSearch, soundl, xnxxdl, ytsearch, ytmp3, ytmp4 } = require("./SumiFX")
 
 module.exports = {
     aptoide: aptoide,
@@ -13,6 +13,7 @@ module.exports = {
     spotifySearch: spotifySearch,
     spotify: spotify,
     soundSearch: soundSearch,
+    soundl: soundl,
     tiktokdl: tiktokdl,
     pinterest: pinterest,
     ppcouple: ppcouple,
